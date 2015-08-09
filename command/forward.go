636abd3898cadbfc865849f10b389de93bb26f81
tcnksm <nsd22843@gmail.com>
2015-08-10 01:30:11 +0900

    Add logger

--- command/forward.go
+++ command/forward.go
@@ -1,105 +1,160 @@
 package command
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
-	// This should be temporary, libcompose imports ssh package
+	"github.com/hashicorp/logutils"
 	"github.com/mitchellh/go-homedir"
+
+	// This should be temporary, libcompose imports ssh package
 	"github.com/tcnksm/boot2k8s/vendor/ssh"
 )
 
 const (
-	Boot2DockerBin string = "boot2docker"
+	B2DSshKeyFile string = "id_boot2docker"
+	B2DSshUser    string = "docker"
 )
 
 type ForwardCommand struct {
 	Meta
 }
 
-func (c *ForwardCommand) Run(args []string) int {
-
+// B2DSshAuthMethod return ssh auth method for boot2docker.
+// It reads & parse ssh key file and constuct auth method.
+// If something wrong, returns error.
+func B2DSshAuthMethod() (ssh.AuthMethod, error) {
 	home, err := homedir.Dir()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	keyPath := filepath.Join(home, ".ssh", "id_boot2docker")
+	// Read default file
+	keyPath := filepath.Join(home, ".ssh", B2DSshKeyFile)
 	buff, err := ioutil.ReadFile(keyPath)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	key, _ := ssh.ParsePrivateKey(buff)
+	signer, _ := ssh.ParsePrivateKey(buff)
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+
+	return ssh.PublicKeys(signer), nil
+}
+
+func (c *ForwardCommand) Run(args []string) int {
+
+	// Proxy forwarding is only for OS which need boot2docker
+	// to run docker.
+	if runtime.GOOS != "darwin" {
+		c.Ui.Error("You don't need to run port forwarding")
+		return 1
+	}
+
+	logLevel := "debug"
+
+	// Create logger with Log level
+	logger := log.New(&logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "INFO", "WARN", "ERROR"},
+		MinLevel: (logutils.LogLevel)(strings.ToUpper(logLevel)),
+		Writer:   os.Stderr,
+	}, "", log.LstdFlags)
+
+	logger.Printf("[DEBUG] LogLevel: %s", logLevel)
+
+	authMethod, err := B2DSshAuthMethod()
+	if err != nil {
+		c.Ui.Error(fmt.Sprintf(
+			"Failed to construct ssh auth method for boot2docker: %s", err))
+		return 1
 	}
 
 	cfg := &ssh.ClientConfig{
-		User: "docker",
+		User: B2DSshUser,
 		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(key),
+			authMethod,
 		},
 	}
 
 	// Establish connection with SSH server
-	sshConn, err := ssh.Dial("tcp", "localhost:2022", cfg)
+	sshServer := "localhost:2022"
+	sshConn, err := ssh.Dial("tcp", sshServer, cfg)
 	if err != nil {
-		panic(err)
+		c.Ui.Error(fmt.Sprintf(
+			"Failed to establish connection with SSH server %s: %s", sshServer, err))
+		return 1
 	}
 	defer sshConn.Close()
+	logger.Printf("[DEBUG] Establish connection with SSH server %s", sshServer)
 
 	// Start local server to forward traffic to remote server
-	localListener, err := net.Listen("tcp", "localhost:8080")
+	localServer := "localhost:8080"
+	localListener, err := net.Listen("tcp", localServer)
 	if err != nil {
-		panic(err)
+		c.Ui.Error(fmt.Sprintf(
+			"Failed to start local server %s: %s", localServer, err))
+		return 1
 	}
 	defer localListener.Close()
+	logger.Printf("[INFO] Start local server")
 
 	for {
+
+		logger.Printf("[INFO] Listening on %s", localServer)
+		// Accept requst and start local connection
+		localConn, err := localListener.Accept()
+		if err != nil {
+			c.Ui.Error(fmt.Sprintf(
+				"Failed to accept request: %s", err))
+			return 1
+		}
+		logger.Printf("[INFO] Accept request")
 
 		// Establish connection with remote server via SSH connection
 		sshRemoteConn, err := sshConn.Dial("tcp", "localhost:8080")
 		if err != nil {
-			panic(err)
+			c.Ui.Error(fmt.Sprintf(
+				"Failed to establish connection with remote server on boot2docker: %s\n"+
+					"This error happends when kubelet is not working. Check it via `docker ps` command.", err))
+			return 1
 		}
-
-		// Accept requst and start local connection
-		localConn, err := localListener.Accept()
-		if err != nil {
-			panic(err)
-		}
-
-		log.Printf("Accepted connection %v\n", localConn)
 
 		doneCh := make(chan struct{})
 
-		// Start data transfer from remote to local
 		go func() {
+			logger.Printf("[DEBUG] Start data transfer from remote server to local")
 			_, err := io.Copy(localConn, sshRemoteConn)
 			if err != nil {
-				panic(err)
+				logger.Printf("[ERROR] Failed to transfer from remote server to local: %s", err)
 			}
 			doneCh <- struct{}{}
 		}()
 
-		// Start data transfer from local to remote
 		go func() {
+			logger.Printf("[DEBUG] Start data transfer from local server to remote")
 			_, err := io.Copy(sshRemoteConn, localConn)
 			if err != nil {
-				panic(err)
+				logger.Printf("[ERROR] Failed to transfer from local server to remote: %s", err)
 			}
 			doneCh <- struct{}{}
 		}()
 
 		<-doneCh
 
+		// Close all connections
 		localConn.Close()
 		sshRemoteConn.Close()
+
+		logger.Printf("[INFO] Finish forwarding")
 	}
 
 	return 0
