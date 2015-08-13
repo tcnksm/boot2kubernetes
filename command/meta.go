243d0c60d300fe5ffc4b3afdbaf9151132883ee7
tcnksm <nsd22843@gmail.com>
2015-08-13 17:58:28 +0900

    Add logrus discard

--- command/meta.go
+++ command/meta.go
@@ -1,6 +1,17 @@
 package command
 
-import "github.com/mitchellh/cli"
+import (
+	"io/ioutil"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/mitchellh/cli"
+)
+
+func init() {
+	// libcomopse depends on logrus and it generates its log.
+	// So stop generating it from here.
+	logrus.SetOutput(ioutil.Discard)
+}
 
 // Meta contain the meta-option that nearly all subcommand inherited.
 type Meta struct {
