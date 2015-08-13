67a255dc1b642714ebb7b463f66e62f14c567c9c
tcnksm <nsd22843@gmail.com>
2015-08-13 17:32:34 +0900

    Add version check

--- command/version.go
+++ command/version.go
@@ -3,6 +3,9 @@ package command
 import (
 	"bytes"
 	"fmt"
+	"time"
+
+	"github.com/tcnksm/go-latest"
 )
 
 type VersionCommand struct {
@@ -14,14 +17,38 @@ type VersionCommand struct {
 }
 
 func (c *VersionCommand) Run(args []string) int {
-	var versionString bytes.Buffer
 
+	// Start goroutine to check latest version on Github
+	verCheckCh := make(chan *latest.CheckResponse)
+	go func() {
+		githubTag := &latest.GithubTag{
+			Owner:      "tcnksm",
+			Repository: "boot2kubernetes",
+		}
+
+		// Ignore error, because it's not important
+		res, _ := latest.Check(githubTag, c.Version)
+		verCheckCh <- res
+	}()
+
+	var versionString bytes.Buffer
 	fmt.Fprintf(&versionString, "%s version %s", c.Name, c.Version)
 	if c.Revision != "" {
 		fmt.Fprintf(&versionString, " (%s)", c.Revision)
 	}
 
 	c.Ui.Output(versionString.String())
+
+	select {
+	case res := <-verCheckCh:
+		if res != nil && !res.Latest {
+			c.Ui.Error(fmt.Sprintf(
+				"Latest version of %s is %s, please update it", c.Name, res.Current))
+		}
+	case <-time.After(2 * time.Second):
+		// Terminate version check soon
+	}
+
 	return 0
 }
 
